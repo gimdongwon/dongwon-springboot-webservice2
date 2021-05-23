406627c185da5e32604de36353811980955e22b1
gimdongwon <ehddnjs8989@gmail.com>
2021-05-23 14:35:11 +0900

    [3장] jpa를 이용한 날짜 추가

--- src/main/java/com/dongwon/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/dongwon/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.dongwon.book.springboot.domain.posts;
 
+import com.dongwon.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor // 기본 생성자 자동 추가, public Posts(){}와 같은 효과
 @Entity // 테이블과 링크될 클래스, 언더스코어 네이밍 테이블 이름과 매칭함
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
