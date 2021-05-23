a8658640eeff085318c6f801d9ed728ca6db9142
gimdongwon <ehddnjs8989@gmail.com>
2021-05-23 12:07:36 +0900

    [3장] 수정 조회 코드 작성완료

--- src/main/java/com/dongwon/book/springboot/web/PostsApiController.java
+++ src/main/java/com/dongwon/book/springboot/web/PostsApiController.java
@@ -1,7 +1,9 @@
 package com.dongwon.book.springboot.web;
 
 import com.dongwon.book.springboot.service.posts.PostsService;
+import com.dongwon.book.springboot.web.dto.PostsResponseDto;
 import com.dongwon.book.springboot.web.dto.PostsSaveRequestDto;
+import com.dongwon.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +18,7 @@ public class PostsApiController {
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsSaveRequestDto requestDto){
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
     }
 
--- src/main/java/com/dongwon/book/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/dongwon/book/springboot/web/dto/PostsResponseDto.java
@@ -1,5 +1,19 @@
 package com.dongwon.book.springboot.web.dto;
 
-public class PostsResponseDto {
+import com.dongwon.book.springboot.domain.posts.Posts;
+import lombok.Getter;
 
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entiry){
+        this.id = entiry.getId();
+        this.title = entiry.getTitle();
+        this.content = entiry.getContent();
+        this.author = entiry.getAuthor();
+    }
 }
