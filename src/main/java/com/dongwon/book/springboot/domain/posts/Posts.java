a0ca759d0fba4bc0a284f6f93f400ecbf4a6ccb2
gimdongwon <ehddnjs8989@gmail.com>
2021-05-23 10:55:59 +0900

    [3장] JPA 연결 및 등록 조회 확인

---
+++ src/main/java/com/dongwon/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.dongwon.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가, public Posts(){}와 같은 효과
+@Entity // 테이블과 링크될 클래스, 언더스코어 네이밍 테이블 이름과 매칭함
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "Text", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자의 상단 선언시 생성자에 포함된 필드만 빌더에 포함.
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
