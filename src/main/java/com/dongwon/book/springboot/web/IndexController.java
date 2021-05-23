b3f96b5f7cef5f6ae648adae49b2fb8b9372f5d0
gimdongwon <ehddnjs8989@gmail.com>
2021-05-24 04:16:07 +0900

    [4장] mushtache update 구현완료

--- src/main/java/com/dongwon/book/springboot/web/IndexController.java
+++ src/main/java/com/dongwon/book/springboot/web/IndexController.java
@@ -1,12 +1,15 @@
 package com.dongwon.book.springboot.web;
 
 import com.dongwon.book.springboot.service.posts.PostsService;
+import com.dongwon.book.springboot.web.dto.PostsListResponseDto;
+import com.dongwon.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +24,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
