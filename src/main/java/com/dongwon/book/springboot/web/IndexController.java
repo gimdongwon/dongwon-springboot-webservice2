ce9b88dfdd3b8b1e4f88424e24d81fbd2463a578
gimdongwon <ehddnjs8989@gmail.com>
2021-05-25 04:37:57 +0900

    [5장] 구글 로그인 구현중 에러 발생

--- src/main/java/com/dongwon/book/springboot/web/IndexController.java
+++ src/main/java/com/dongwon/book/springboot/web/IndexController.java
@@ -1,24 +1,32 @@
 package com.dongwon.book.springboot.web;
 
+import com.dongwon.book.springboot.config.auth.dto.SessionUser;
+import com.dongwon.book.springboot.domain.user.User;
 import com.dongwon.book.springboot.service.posts.PostsService;
-import com.dongwon.book.springboot.web.dto.PostsListResponseDto;
+
 import com.dongwon.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
@@ -32,4 +40,5 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
+
 }
